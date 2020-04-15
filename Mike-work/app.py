04d5b485dc490dfb2bd1f71bb7dbbb7e6c32d6cc
Cowdog90 <57324588+Cowdog90@users.noreply.github.com>
2020-04-14 19:26:43 -0500

    flask

--- Mike-work/app.py
+++ Mike-work/app.py
@@ -17,7 +17,7 @@ def get_all_docs():
   star = mongo.project2.aviation.find()
   output = []
   for s in star:
-    output.append({'index' : s['index'], 'Event_Id' : s['Event_Id'], 'Event_Date' : s['Event_Date'], 'Location' : s['Location'], 'Country' : s['Country'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude'], 'Airport_Code' : s['Airport_Code'], 'Injury_Severity' : s['Injury_Severity'], 'Aircraft_Damage' : s['Aircraft_Damage'], 'Aircraft_Category' : s['Aircraft_Category'], 'Make' : s['Make'], 'Model' : s['Model'], 'Total_Fatal_Injuries' : s['Total_Fatal_Injuries'], 'Total_Serious_Injuries' : s['Total_Serious_Injuries'], 'Total_Minor_Injuries' : s['Total_Minor_Injuries'], 'Total_Uninjured' : s['Total_Uninjured'], 'Broad_Phase_of_Flight' : s['Broad_Phase_of_Flight']})
+    output.append({'index' : s['index'], 'Event_Id' : s['Event_Id'], 'Event_Date' : s['Event_Date'], 'Location' : s['Location'], 'Country' : s['Country'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude'], 'Airport_Code' : s['Airport_Code'], 'Injury_Severity' : s['Injury_Severity'], 'Aircraft_Damage' : s['Aircraft_Damage'], 'Aircraft_Category' : s['Aircraft_Category'], 'Make' : s['Make'], 'Model' : s['Model'], 'Total_Fatal_Injuries' : s['Total_Fatal_Injuries'], 'Total_Serious_Injuries' : s['Total_Serious_Injuries'], 'Total_Minor_Injuries' : s['Total_Minor_Injuries'], 'Total_Uninjured' : s['Total_Uninjured'], 'Broad_Phase_of_Flight' : s['Broad_Phase_of_Flight'], 'Damage_Rating' : s['Damage_Rating']})
   return jsonify({'result' : output})
 
 
