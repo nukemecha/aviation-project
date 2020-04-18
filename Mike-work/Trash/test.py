8119073f2b5adff0f90b90820d23f908493ee60b
Cowdog90 <57324588+Cowdog90@users.noreply.github.com>
2020-04-17 20:05:19 -0500

    final

--- Mike-work/Trash/test.py
+++ Mike-work/Trash/test.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def index():
     movie_list = ["Mighty Ducks", "Space Jam", "Clerks", "Batman", "Avengers"]
     return ("index.html")
+
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- Mike-work/app.py
+++ Mike-work/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 import pymongo as PM
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,10 +16,18 @@ mongo = PM.MongoClient('mongodb://18.190.0.114:27017/')
 @app.route('/', methods=['GET'])
 def get_all_docs():
   star = mongo.project2.aviation.find()
+  #data = pd.DataFrame(list(collection.find()))
   output = []
   for s in star:
     output.append({'index' : s['index'], 'Event_Id' : s['Event_Id'], 'Event_Date' : s['Event_Date'], 'Location' : s['Location'], 'Country' : s['Country'], 'Latitude' : s['Latitude'], 'Longitude' : s['Longitude'], 'Airport_Code' : s['Airport_Code'], 'Injury_Severity' : s['Injury_Severity'], 'Aircraft_Damage' : s['Aircraft_Damage'], 'Aircraft_Category' : s['Aircraft_Category'], 'Make' : s['Make'], 'Model' : s['Model'], 'Total_Fatal_Injuries' : s['Total_Fatal_Injuries'], 'Total_Serious_Injuries' : s['Total_Serious_Injuries'], 'Total_Minor_Injuries' : s['Total_Minor_Injuries'], 'Total_Uninjured' : s['Total_Uninjured'], 'Broad_Phase_of_Flight' : s['Broad_Phase_of_Flight'], 'Damage_Rating' : s['Damage_Rating']})
   return jsonify({'result' : output})
+
+
+@app.route('/jin', methods=['GET'])
+def get_jin():
+  df = pd.read_csv('location_final.csv')
+  df = df.to_json(orient='records')
+  return df
 
 
 
